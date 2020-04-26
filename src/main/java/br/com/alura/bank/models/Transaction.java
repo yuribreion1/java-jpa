dfb56dcda7f5a9e6128bb1ed2b8f6eae3bb97076
Yuri Breion <yuribreion@br.ibm.com>
2020-04-25 22:14:59 -0300

    Transaction table

---
+++ src/main/java/br/com/alura/bank/models/Transaction.java
@@ -0,0 +1,76 @@
+package br.com.alura.bank.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private BigDecimal value;
+	@Enumerated(EnumType.STRING)
+	private TransactionType type;
+	private LocalDateTime currentDate;
+	private String description;
+
+	@ManyToOne
+	private Account account;
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public TransactionType getType() {
+		return type;
+	}
+
+	public void setType(TransactionType type) {
+		this.type = type;
+	}
+
+	public LocalDateTime getCurrentDate() {
+		return currentDate;
+	}
+
+	public void setCurrentDate(LocalDateTime currentDate) {
+		this.currentDate = currentDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
