b880f6707365d1cfba02e6494a191089e5290e00
Yuri Breion <yuribreion@br.ibm.com>
2020-05-02 00:27:52 -0300

    Update project

--- src/main/java/br/com/alura/bank/test/TestTableAccount.java
+++ src/main/java/br/com/alura/bank/test/TestTableAccount.java
@@ -1,13 +1,12 @@
 package br.com.alura.bank.test;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class TestTableAccount {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("bank");
-		EntityManager createEntityManager = emf.createEntityManager();
+//		EntityManager createEntityManager = emf.createEntityManager();
 		emf.close();
 	}
 }
